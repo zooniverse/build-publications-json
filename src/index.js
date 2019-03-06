c79e3be9152a7543681bc4d367a27c2a4f4a77e1
Roger Hutchings <roger@hutchin.gs>
2019-03-06 14:09:43 +0000

    remove unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 
-const createFile = require('./create-file')
 const logger = require('./create-logger')(__filename)
 const fetchPublications = require('./fetch-publications')
 const formatData = require('./format-data')
@@ -12,8 +11,6 @@ async function main () {
   logger.info(`Logger set to level: ${logger.level}`)
   const publications = await fetchPublications().catch(logger.error)
   const formattedPublicationsData = formatData(publications)
-  // const filePath = createFile(formattedPublications)
-  // const filePath = ''
   await saveToS3(formattedPublicationsData)
   logger.info('Finished!')
 }
--- src/save-to-s3.js
+++ src/save-to-s3.js
@@ -8,6 +8,7 @@ AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   },
+  logger,
   region: process.env.S3_REGION
 })
 
@@ -19,15 +20,12 @@ async function saveToS3 (dataObject) {
   const params = {
     Bucket: process.env.S3_BUCKET,
     Key: process.env.S3_KEY || 'publications/publications.json',
-    Body: Buffer.from(JSON.stringify(dataObject, null, 2)),
+    Body: JSON.stringify(dataObject, null, 2),
     ACL: 'public-read'
   }
 
   await s3
     .putObject(params)
     .promise()
-    .catch(error => {
-      logger.error(error.message)
-      console.error(error)
-    })
+
 }
